0dc0b9a5dea6c6b891a6ae46899fed3507574a64
KudrekoPavlo <pavlo.kudrenko@etu.univ-lyon1.fr>
2024-12-02 16:57:15 +0100

    Add files via upload

--- proj/serial_critique/controleurs/partie.py
+++ proj/serial_critique/controleurs/partie.py
@@ -7,8 +7,18 @@ if '4_briques' not in SESSION:
     SESSION['4_briques'] = [get_random_brique(SESSION['CONNEXION']) for _ in range(4)]
 
 
+if 'max_tours' not in SESSION:
+    SESSION['max_tours'] = int(10)
 
-    
+if 'maxmouv' in GET:
+    try:
+        max_tours = int(GET['maxmouv'][0]) + 1
+        SESSION['max_tours'] = max_tours
+    except ValueError:
+        REQUEST_VARS['maxt_e'] = "La valeur de tour maaximal n`est pas bon"
+
+
+       
 if 'y' in GET and 'x' in GET:
     try:
         taille_y = int(GET['y'][0])
@@ -23,11 +33,32 @@ if 'y' in GET and 'x' in GET:
 
     
 if 'brique' in GET:
+    SESSION['max_tours'] -=  1
     try:
-        idb = int(GET['brique'][0])  # Отримуємо ID цеглини
+        idb = int(GET['brique'][0])  
         for i, brique in enumerate(SESSION['4_briques']):
             if brique[0] == idb:
-                SESSION['4_briques'][i] = get_random_brique(SESSION['CONNEXION'])  # Замінюємо обрану цеглину
+                SESSION['4_briques'][i] = get_random_brique(SESSION['CONNEXION'])  
                 break
     except (ValueError, IndexError):
         REQUEST_VARS['error'] = "Erreur lors de la sélection de la brique."      
+
+    
+
+
+if 1 == SESSION['max_tours'] : 
+    SESSION['perdr'] = "aff"
+
+if 'NewGame' in GET:
+    SESSION['4_briques'] = [get_random_brique(SESSION['CONNEXION']) for _ in range(4)]
+    SESSION['max_tours'] = 0
+    REQUEST_VARS['maxt_e'] = 0
+    SESSION['grid'] = 0
+    SESSION['tailles'] = 0
+    REQUEST_VARS['tailles_e'] = 0
+    REQUEST_VARS['error'] = 0
+    SESSION['perdr'] = 0
+
+
+
+
